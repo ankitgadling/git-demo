77abda8fa171810993e14970be10d74e431dd033
Ankit Gadling <ankitgadling2@gmail.com>
2026-01-07 20:58:08 +0530

    added helper function

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
 # This file will be used to add utitlity fucntions in future
 
 print("utils file")
+
+def helper_function():
+    print("This is a helper function in utils.py")
+    return "Helper function executed"
+
+if __name__ == "__main__":
+    result = helper_function()
+    print(result)
