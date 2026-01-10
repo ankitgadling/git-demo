0e754c08aa4268cd39c945e1bea80fafb803eacd
Ankit Gadling <ankitgadling2@gmail.com>
2026-01-10 20:20:54 +0530

    add numberic function in utils

--- utils.py
+++ utils.py
@@ -9,3 +9,17 @@ def helper_function():
 def another_util_function():
     print("This is another utility function in utils.py")
     return "Another utility function executed"
+
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_difference(a, b):
+    return a - b
+
+def calculate_product(a, b):
+    return a * b
+
+def calculate_quotient(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
